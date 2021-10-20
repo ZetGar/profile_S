c0edeb15c1f1562cbab3789a7395c572c366aaea
ZetGar <88043986+ZetGar@users.noreply.github.com>
2021-10-20 14:54:49 +0900

    Update function_main.js

--- js/function_main.js
+++ js/function_main.js
@@ -1,92 +1,84 @@
-$(function(){
-
+$(function () {
   // header영역
-  $(window).on('scroll', function(){
-    const $header = $('header');
-    $('header').toggleClass('sticky', window.scrollY > 0);
+  $(window).on("scroll", function () {
+    const $header = $("header");
+    $("header").toggleClass("sticky", window.scrollY > 0);
   });
 
-  $('.total').on('click',function(){
-    $(this).toggleClass('on');
-    
-    if($('.total').hasClass('on')){
-      $('nav').css({
-        right: '-7%'
+  $(".total").on("click", function () {
+    $(this).toggleClass("on");
+
+    if ($(".total").hasClass("on")) {
+      $("nav").css({
+        right: "0",
       });
     } else {
-      $('nav').css({
-        right: '-500px'
+      $("nav").css({
+        right: "-500px",
       });
     }
   });
 
-  $('.logo>a').on('click',function(evt){
+  $(".logo>a").on("click", function (evt) {
     evt.preventDefault();
 
-    $('html,body').stop().animate({
-      scrollTop:0
+    $("html,body").stop().animate({
+      scrollTop: 0,
     });
   });
 
-  $(window).on('scroll', function(){
+  $(window).on("scroll", function () {
     const scrollpos = $(window).scrollTop();
-    $('.swot').css({
-     'max-height' : + (scrollpos) + 'px'
+    $(".swot").css({
+      "max-height": +scrollpos + "px",
     });
   });
-
 
   // main 영역
-  document.addEventListener('mousemove', function(e){
-    let body = document.querySelector('body');
-    let bubbles = document.createElement('span');
+  document.addEventListener("mousemove", function (e) {
+    let body = document.querySelector("body");
+    let bubbles = document.createElement("span");
     let x = e.offsetX;
     let y = e.offsetY;
 
-    bubbles.style.left = x + 'px';
-    bubbles.style.top = y + 'px';
+    bubbles.style.left = x + "px";
+    bubbles.style.top = y + "px";
 
-    let size = Math.random () * 30;
-    bubbles.style.width = size + 'px';
-    bubbles.style.height = size + 'px';
+    let size = Math.random() * 30;
+    bubbles.style.width = size + "px";
+    bubbles.style.height = size + "px";
 
     body.appendChild(bubbles);
 
-    setTimeout(function(){
+    setTimeout(function () {
       bubbles.remove();
-    },3000);
+    }, 3000);
   });
 
+  const $pagination = $(".pf_pagination>li");
+  const $slides = $(".pf_slides");
 
-  const $pagination = $('.pf_pagination>li');
-  const $slides = $('.pf_slides');
-
-  let nowIdx=0;
+  let nowIdx = 0;
   let intervalKey;
 
-  const moveFn = function(){
-    $pagination.eq(nowIdx).addClass('on').siblings().removeClass('on');
-    $slides.stop().animate({left: -158*nowIdx});
+  const moveFn = function () {
+    $pagination.eq(nowIdx).addClass("on").siblings().removeClass("on");
+    $slides.stop().animate({ left: -158 * nowIdx });
   };
 
-  $(window).on('load', function(){    
-
+  $(window).on("load", function () {
     nowIdx = $pagination.index($(this));
-    
-    intervalKey = setInterval(() => {
 
-      if(nowIdx<3){
+    intervalKey = setInterval(() => {
+      if (nowIdx < 3) {
         nowIdx++;
-      }else{
-        nowIdx=0;
+      } else {
+        nowIdx = 0;
       }
 
       moveFn();
-
     }, 3000);
   });
-  
-
 
   // sns영역
   // setInterval(function(){
@@ -100,7 +92,6 @@ $(function(){
 
   //   $('.cat >p').eq(nowIdx).children('img').fadeIn();
 
-    
   // },2000);
 
   // // hello 영역
@@ -114,30 +105,27 @@ $(function(){
   //   });
   // }, 1000);
 
+  // popup_contact
+  $("#popup").toggleClass("on");
 
-  
-    // popup_contact
-    $('#popup').toggleClass('on');
-
-    $('.gnb>li').eq(3).on('click', function(){
-      if($('#popup').hasClass('on')){
-      $('#popup').show();
-      $('.gnb>li').eq(3).addClass('on').siblings().removeClass('on');
+  $(".gnb>li")
+    .eq(3)
+    .on("click", function () {
+      if ($("#popup").hasClass("on")) {
+        $("#popup").show();
+        $(".gnb>li").eq(3).addClass("on").siblings().removeClass("on");
       }
     });
 
-    $('.close').on('click',function(){
-      $('#popup').hide();
-      $('.gnb>li').eq(0).addClass('on').siblings().removeClass('on');
-    });
+  $(".close").on("click", function () {
+    $("#popup").hide();
+    $(".gnb>li").eq(0).addClass("on").siblings().removeClass("on");
+  });
 
-    $('#popup').on('click',function(e){
-      if(e.target == e.currentTarget){
-        $('#popup').hide();
-        $('.gnb>li').eq(0).addClass('on').siblings().removeClass('on');
-      }
-    });
-
-    
-
+  $("#popup").on("click", function (e) {
+    if (e.target == e.currentTarget) {
+      $("#popup").hide();
+      $(".gnb>li").eq(0).addClass("on").siblings().removeClass("on");
+    }
+  });
 });
